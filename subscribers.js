eebfa2f6422c7b052505aa8a3c2be508c1e0c77e
Ampte B Marak <amptebmarak@gmail.com>
2025-10-01 23:10:36 +0530

    Check email if exists

--- subscribers.js
+++ subscribers.js
@@ -6,14 +6,22 @@ router.post("/api/add-subscribers", (req, res) => {
 
     const {email} = req.body;
 
+    const checkEmail = "SELECT * FROM subscribers WHERE email = ?";
+    const proccessEmail = db.prepare(checkEmail);
+    const resultEmail = proccessEmail.get(email);
+
+    if(resultEmail){
+        res.json({subscribe : false, message: 'Already subscribed!'});
+    };
+
     const sql = "INSERT INTO subscribers(email) VALUES(?)";
-
     const proccess = db.prepare(sql);
-
     const result = proccess.run(email);
 
-    if(result){
-        res.json({ subscribe : true});
+    if(result.length > 0){
+        res.json({ subscribe : true, message: 'Subscribed successfully'});
+    }else{
+        res.json({subscribe : false, message: 'Subscribtion failed!'});
     };
 });
 
